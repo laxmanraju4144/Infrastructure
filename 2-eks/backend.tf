4aa19f079b860fe661487bd918ae23143dc730b5
Ganapathi Laxman Raju <95155699+laxmanraju4144@users.noreply.github.com>
2026-04-10 13:41:34 +0530

    Update backend.tf

--- 2-eks/backend.tf
+++ 2-eks/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "quantamvector-infra-statefile-backup"
     key            = "quantamvector/2-eks/terraform.tfstate"
-    region         = "ap-northeast-1"
+    region         = "us-east-1"
     dynamodb_table = "quantamvector-terraform-locks"
     encrypt        = true
   }
