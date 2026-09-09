bca2a28da25200f67b2bb2b7b8e5c365f7a1047f
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-09-09 11:35:45 +0530

    latets chnages

--- 1-network/variables.tf
+++ 1-network/variables.tf
@@ -1,16 +1,19 @@
 variable "project" {
   type        = string
   description = "Project name used for resource tags"
+  default     = "microservices-v2"
 }
 
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 
 variable "azs" {
-  type    = list(string)
-  default = ["us-east-1a", "us-east-1b"]
+  type        = list(string)
+  description = "List of availability zones"
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 
